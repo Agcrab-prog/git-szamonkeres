9bf114338b530f108b861576de8634d8d718572a
Agcrab-prog <magda.agoston123@gmail.com>
2023-12-18 12:49:02 +0100

    legnagyobb szám kiiratása

--- git-szamonkeres/Program.cs
+++ git-szamonkeres/Program.cs
@@ -35,5 +35,19 @@ class Program
             }
         }
         Console.WriteLine($"A legkisebb szam: {minSzam}");
+
+
+        // 5. Feladat: Maximum kiválasztás és kiírás
+        int maxSzam = szamok.Count > 0 ? szamok[0] : 0;
+        foreach (var elem in szamok)
+        {
+            if (elem > maxSzam)
+            {
+                maxSzam = elem;
+            }
+        }
+        Console.WriteLine($"A legnagyobb szam: {maxSzam}");
+
+        Console.ReadKey();
     }
 }
